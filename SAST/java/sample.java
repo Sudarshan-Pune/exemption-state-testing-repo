457490ec276cdc8346e47103132ce43237207304
Akshay Khajuriya <241728994+cx-akshay-khajuriya@users.noreply.github.com>
2026-07-07 17:52:18 +0530

    Update sample.java

--- SAST/java/sample.java
+++ SAST/java/sample.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Sample {
     public static void main(String[] args) throws IOException {
 
-        // Hardcoded credential (SAST finding)
+        // Hardcoded credential (SAST finding)!!
         String password = "sensitivePassword";
         System.out.println("Received password: " + password);
 
