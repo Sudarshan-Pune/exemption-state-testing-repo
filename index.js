013cfb0c5ee6ede1f05553f472ed722a93f98a68
Akshay Khajuriya <241728994+cx-akshay-khajuriya@users.noreply.github.com>
2026-07-07 15:04:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { exec } = require("child_process");
 
 const app = express();
-
+console.log("new log")
 app.get("/ping", (req, res) => {
     const host = req.query.host;
     exec("ping " + host, (err, stdout) => {
