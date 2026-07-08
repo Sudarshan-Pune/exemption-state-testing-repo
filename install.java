2bc6a2dbd2f7d0907267ecfcb3144a711ca767f1
Akshay Khajuriya <241728994+cx-akshay-khajuriya@users.noreply.github.com>
2026-07-08 16:36:35 +0530

    Update install.java

--- install.java
+++ install.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 public class Sample {
     public static void main(String[] args) throws IOException {
-        //!! Hardcdoded dsdsccxcccredential (SAST finding)!!!!!
+        //!! Hardcdoded dsdsccxcccredentia;l (SAST finding)!!!!!
         String password = "sensitivePassword";
         System.out.println("Received password: " + password);
         // Command Injection (SAST finding)[
